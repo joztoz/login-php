8fbcc0774022cb85a9080e4c914de70901b7ae60
joztoz <52895941+joztoz@users.noreply.github.com>
2020-04-07 14:14:10 +0200

    Create login.php
    this is working php login

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://eu5.fusionsolar.huawei.com/thirdData/login",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS =>"{\n    \"userName\": \"username\",\n    \"systemCode\": \"password\"\n}",
+  CURLOPT_HTTPHEADER => array(
+    "Content-Type: application/json"
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
